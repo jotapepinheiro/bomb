939065135d4a3debbf5b2fb126472637684113b0
Joao Paulo Pinheiro <joaopinheiro.ti@gmail.com>
2021-12-30 03:36:44 -0300

    3 contas ubuntu

---
+++ bomb/test-loop.py
@@ -0,0 +1,30 @@
+
+accounts = [
+    {
+        "conta": 1,
+        "login": 0,
+        "heroes": 0,
+        "new_map": 0,
+        "refresh_heroes": 0,
+        "check_mult": 0
+    },
+    {
+        "conta": 2,
+        "login": 0,
+        "heroes": 0,
+        "new_map": 0,
+        "refresh_heroes": 0,
+        "check_mult": 0
+    },
+    {
+        "conta": 3,
+        "login": 0,
+        "heroes": 0,
+        "new_map": 0,
+        "refresh_heroes": 0,
+        "check_mult": 0
+    }
+]
+
+for last in accounts:
+    print(last["conta"])
